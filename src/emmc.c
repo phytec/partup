150d15cfee1d30a40c90c4efa57ef664713908de
Martin Schwan <m.schwan@phytec.de>
2022-12-05 15:43:53 +0100

    src: emmc: Check for errors when extracting and copying data
    Check for errors and return if failing to extract archives or copying
    files to their respective partitions.
    
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

--- src/emmc.c
+++ src/emmc.c
@@ -271,7 +271,8 @@ pu_emmc_write_data(PuFlash *flash,
             if (g_regex_match_simple(".tar", path, G_REGEX_CASELESS, 0)) {
                 g_debug("Extracting '%s' to '%s'", path, part_mount);
                 pu_mount(part_path, part_mount);
-                pu_archive_extract(path, part_mount, error);
+                if (!pu_archive_extract(path, part_mount, error))
+                    return FALSE;
                 pu_umount(part_mount);
             } else if (g_regex_match_simple(".ext[234]$", path, 0, 0)) {
                 g_debug("Writing '%s' to '%s'", path, part_mount);
@@ -283,7 +284,8 @@ pu_emmc_write_data(PuFlash *flash,
             } else {
                 g_debug("Copying '%s' to '%s'", path, part_mount);
                 pu_mount(part_path, part_mount);
-                pu_file_copy(path, part_mount, error);
+                if (!pu_file_copy(path, part_mount, error))
+                    return FALSE;
                 pu_umount(part_mount);
             }
 
