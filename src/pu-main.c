23b45dcbc8d6c98454e6564c15887595489a8015
Martin Schwan <m.schwan@phytec.de>
2023-03-17 14:13:11 +0100

    WIP: main: commands
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

--- src/pu-main.c
+++ src/pu-main.c
@@ -11,6 +11,7 @@
 #include "pu-emmc.h"
 #include "pu-flash.h"
 #include "pu-mount.h"
+#include "pu-package.h"
 #include "pu-utils.h"
 #include "pu-version.h"
 
@@ -18,6 +19,7 @@ static gboolean arg_debug = FALSE;
 static gboolean arg_version = FALSE;
 static gboolean arg_skip_checksums = FALSE;
 static gchar *arg_device = NULL;
+static gchar *arg_package = NULL;
 
 GPtrArray *arg_remaining = NULL;
 
@@ -27,8 +29,6 @@ arg_parse_remaining(const gchar *option_name,
                     G_GNUC_UNUSED gpointer data,
                     GError **error)
 {
-    g_debug("%s(option_name=\"%s\" value=\"%s\")", __func__, option_name, value);
-
     g_ptr_array_add(arg_remaining, g_strdup(value));
 
     return TRUE;
@@ -49,6 +49,43 @@ static GOptionEntry option_entries[] = {
     { NULL }
 };
 
+static const gchar * const *commands = {
+    "install",
+    "package",
+    "show"
+};
+static const gchar *description = "\
+Commands:\n\
+  install PACKAGE DEVICE   Install a partup PACKAGE to DEVICE\n\
+  package PACKAGE FILES…   Create a partup PACKAGE with the contents FILES\n\
+  show PACKAGE             List the contents of a partup PACKAGE\n\
+\n\
+Report any issues at <https://github.com/phytec/partup>\
+";
+
+static gboolean
+parse_commands(GPtrArray *args,
+               GError **error)
+{
+    gchar *cmd;
+
+    g_return_val_if_fail(args != NULL, FALSE);
+    g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+
+    if (!g_strv_contains(commands, g_ptr_array_index(args, 0))) {
+        g_set_error(error, PU_ERROR, PU_ERROR_FAILED,
+                    "Unknown command '%s'");
+        return FALSE;
+    }
+    cmd = g_ptr_array_index(args, 0);
+
+    for (guint i = 0; i < args->len; i++) {
+        
+    }
+
+    return TRUE;
+}
+
 int
 main(G_GNUC_UNUSED int argc,
      char **argv)
@@ -56,52 +93,55 @@ main(G_GNUC_UNUSED int argc,
     g_autoptr(GError) error = NULL;
     g_autoptr(GOptionContext) context = NULL;
     g_autoptr(PuConfig) config = NULL;
+    g_autofree gchar *config_path = NULL;
     g_autoptr(PuEmmc) emmc = NULL;
     g_autofree gchar **args;
     gboolean is_mounted;
     gint api_version;
-    const gchar *prog_name = g_path_get_basename(argv[0]);
 
     /* Support unicode filenames */
     args = g_strdupv(argv);
 
+    arg_remaining = g_ptr_array_new();
     context = g_option_context_new(NULL);
     g_option_context_add_main_entries(context, option_entries, NULL);
-    g_option_context_set_description(context, "Report any issues at "
-                                     "<https://github.com/phytec/partup>");
+    g_option_context_set_description(context, description);
     if (!g_option_context_parse_strv(context, &args, &error)) {
         g_printerr("Failed parsing options: %s\n", error->message);
         return 1;
     }
 
-    g_debug("%s", g_strjoinv(" ", (gchar **) arg_remaining->pdata));
-
     if (arg_debug) {
         const gchar *domains = g_getenv("G_MESSAGES_DEBUG");
 
         if (domains != NULL) {
-            g_autofree gchar *new_domains = g_strdup_printf("%s %s", domains, prog_name);
+            g_autofree gchar *new_domains = g_strdup_printf("%s %s", domains, g_get_prgname());
             g_setenv("G_MESSAGES_DEBUG", new_domains, TRUE);
         } else {
-            g_setenv("G_MESSAGES_DEBUG", prog_name, TRUE);
+            g_setenv("G_MESSAGES_DEBUG", g_get_prgname(), TRUE);
         }
     }
 
     if (arg_version) {
-        g_print("%s %s\n", prog_name, PARTUP_VERSION_STRING);
+        g_print("%s %s\n", g_get_prgname(), PARTUP_VERSION_STRING);
         return 0;
     }
 
+    g_debug("%s", g_strjoinv(" ", (gchar **) arg_remaining->pdata));
+    g_debug("command: %s", (gchar *) g_ptr_array_index(arg_remaining, 0));
+
+    return 0;
+
     if (getuid() != 0) {
-        g_printerr("%s must be run as root!\n", prog_name);
+        g_printerr("%s must be run as root!\n", g_get_prgname());
         return 1;
     }
 
-    if (g_strcmp0(arg_config, "") <= 0 || g_strcmp0(arg_device, "") <= 0) {
+    /*if (g_strcmp0(arg_config, "") <= 0 || g_strcmp0(arg_device, "") <= 0) {
         g_printerr("Not enough arguments!\n");
         g_print("%s", g_option_context_get_help(context, TRUE, NULL));
         return 1;
-    }
+    }*/
 
     if (!pu_is_drive(arg_device)) {
         g_printerr("Device '%s' is not a drive!\n", arg_device);
@@ -119,17 +159,18 @@ main(G_GNUC_UNUSED int argc,
     }
 
     // TODO: Mount squashfs image (partup package) to /run/partup
-    if (!pu_package_mount(arg_package, PARTUP_PACKAGE_MOUNT, &error)) {
+    if (!pu_package_mount(arg_package, PU_PACKAGE_PREFIX, &error)) {
         g_printerr("%s\n", error->message);
         return 1;
     }
     // Input files and configuration layout are now available in mounted dir
     // Assign variable, like arg_config, the right paths
+    config_path = g_strdup(PU_PACKAGE_PREFIX "/layout.yaml");
 
-    config = pu_config_new_from_file(arg_config, &error);
+    config = pu_config_new_from_file(config_path, &error);
     if (config == NULL) {
         g_printerr("Failed creating configuration object for file '%s': %s\n",
-                   arg_config, error->message);
+                   config_path, error->message);
         return 1;
     }
     api_version = pu_config_get_api_version(config);
@@ -139,7 +180,7 @@ main(G_GNUC_UNUSED int argc,
         return 1;
     }
 
-    emmc = pu_emmc_new(arg_device, config, arg_prefix, arg_skip_checksums, &error);
+    emmc = pu_emmc_new(arg_device, config, PU_PACKAGE_PREFIX, arg_skip_checksums, &error);
     if (emmc == NULL) {
         g_printerr("Failed parsing eMMC info from config: %s\n", error->message);
         return 1;
@@ -157,12 +198,12 @@ main(G_GNUC_UNUSED int argc,
         g_clear_error(&error);
         if (!pu_umount_all(arg_device, &error))
             g_printerr("Failed unmounting partitions being used by %s: %s\n",
-                       prog_name, error->message);
+                       g_get_prgname(), error->message);
         return 1;
     }
 
-    g_free(arg_config);
     g_free(arg_device);
+    g_free(arg_package);
     g_ptr_array_free(arg_remaining, TRUE);
 
     return 0;
