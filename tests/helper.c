5125c6efa708cd682a95ee1ab2e74c83a3dee320
Leonard Anderweit <l.anderweit@phytec.de>
2023-06-02 16:46:30 +0200

    tests: Test utils functions with root
    Some functions from pu-utils require root access. Only test them when
    the tests are executed as root, otherwise skip them.
    
    Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>

--- tests/helper.c
+++ tests/helper.c
@@ -8,6 +8,7 @@
 #include <gio/gio.h>
 #include <parted/parted.h>
 #include "helper.h"
+#include "pu-glib-compat.h"
 
 GFile *
 create_tmp_file(const gchar *filename,
@@ -45,6 +46,46 @@ empty_file_tear_down(EmptyFileFixture *fixture,
                      G_GNUC_UNUSED gconstpointer user_data)
 {
     g_assert_true(g_file_delete(fixture->part, NULL, &fixture->error));
+    g_assert_no_error(fixture->error);
+    g_assert_cmpint(g_rmdir(fixture->path), ==, 0);
+}
+
+void
+empty_device_set_up(EmptyDeviceFixture *fixture,
+                    G_GNUC_UNUSED gconstpointer user_data)
+{
+    gint wait_status;
+    g_autofree gchar *cmd;
+
+    fixture->error = NULL;
+    fixture->path = g_dir_make_tmp("partup-XXXXXX", &fixture->error);
+    g_assert_no_error(fixture->error);
+    fixture->device = create_tmp_file("device", fixture->path, 100 * PED_MEBIBYTE_SIZE,
+                                      &fixture->error);
+    g_assert_no_error(fixture->error);
+    cmd = g_strdup_printf("losetup -f --show -P %s", g_file_get_path(fixture->device));
+    g_assert_true(g_spawn_command_line_sync(cmd, &fixture->loop_dev, NULL,
+                                            &wait_status, &fixture->error));
+    g_assert_no_error(fixture->error);
+    g_assert_true(g_spawn_check_wait_status(wait_status, &fixture->error));
+    g_assert_no_error(fixture->error);
+    fixture->loop_dev = g_strstrip(fixture->loop_dev);
+}
+
+void
+empty_device_tear_down(EmptyDeviceFixture *fixture,
+                       G_GNUC_UNUSED gconstpointer user_data)
+{
+    gint wait_status;
+    g_autofree gchar *cmd = g_strdup_printf("losetup -d %s", fixture->loop_dev);
+
+    g_assert_true(g_spawn_command_line_sync(cmd, NULL, NULL, &wait_status,
+                                            &fixture->error));
+    g_assert_no_error(fixture->error);
+    g_assert_true(g_spawn_check_wait_status(wait_status, &fixture->error));
+    g_assert_no_error(fixture->error);
+
+    g_assert_true(g_file_delete(fixture->device, NULL, &fixture->error));
     g_assert_no_error(fixture->error);
     g_assert_cmpint(g_rmdir(fixture->path), ==, 0);
 }
--- tests/helper.h
+++ tests/helper.h
@@ -14,6 +14,13 @@ typedef struct {
     gchar *path;
 } EmptyFileFixture;
 
+typedef struct {
+    GError *error;
+    GFile *device;
+    gchar *path;
+    gchar *loop_dev;
+} EmptyDeviceFixture;
+
 GFile * create_tmp_file(const gchar *filename,
                         const gchar *pwd,
                         gsize size,
@@ -22,5 +29,9 @@ void empty_file_set_up(EmptyFileFixture *fixture,
                        G_GNUC_UNUSED gconstpointer user_data);
 void empty_file_tear_down(EmptyFileFixture *fixture,
                           G_GNUC_UNUSED gconstpointer user_data);
+void empty_device_set_up(EmptyDeviceFixture *fixture,
+                         G_GNUC_UNUSED gconstpointer user_data);
+void empty_device_tear_down(EmptyDeviceFixture *fixture,
+                            G_GNUC_UNUSED gconstpointer user_data);
 
 #endif /* PARTUP_TEST_HELPER_H */
