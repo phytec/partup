a5af7c173676642478f9c2a098cb3fc4ff809894
Leonard Anderweit <l.anderweit@phytec.de>
2023-06-22 17:19:02 +0200

    tests: helper: Set filename with user data
    Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>

--- tests/utils.c
+++ tests/utils.c
@@ -78,11 +78,11 @@ test_make_filesystem(EmptyFileFixture *fixture,
     g_autofree gchar *output = NULL;
     gint wait_status;
 
-    g_assert_true(pu_make_filesystem(g_file_get_path(fixture->part), "ext4",
+    g_assert_true(pu_make_filesystem(g_file_get_path(fixture->file), "ext4",
                   &fixture->error));
     g_assert_no_error(fixture->error);
 
-    cmd = g_strdup_printf("blkid -o value -s TYPE %s", g_file_get_path(fixture->part));
+    cmd = g_strdup_printf("blkid -o value -s TYPE %s", g_file_get_path(fixture->file));
     g_assert_true(g_spawn_command_line_sync(cmd, &output, NULL,
                                             &wait_status, &fixture->error));
     g_assert_true(g_spawn_check_wait_status(wait_status, &fixture->error));
@@ -100,11 +100,11 @@ test_write_raw(EmptyFileFixture *fixture,
     PedDevice device;
     device.sector_size = 512;
 
-    g_assert_true(pu_write_raw("data/root.ext4", g_file_get_path(fixture->part),
+    g_assert_true(pu_write_raw("data/root.ext4", g_file_get_path(fixture->file),
                   &device, 0, 0, 0, &fixture->error));
     g_assert_no_error(fixture->error);
 
-    cmd = g_strdup_printf("blkid -o value -s TYPE %s", g_file_get_path(fixture->part));
+    cmd = g_strdup_printf("blkid -o value -s TYPE %s", g_file_get_path(fixture->file));
     g_assert_true(g_spawn_command_line_sync(cmd, &output, NULL,
                                             &wait_status, &fixture->error));
     g_assert_true(g_spawn_check_wait_status(wait_status, &fixture->error));
@@ -118,7 +118,7 @@ test_write_raw_input_offset(EmptyFileFixture *fixture,
 {
     PedDevice device;
     device.sector_size = 512;
-    g_assert_true(pu_write_raw("data/root.ext4", g_file_get_path(fixture->part),
+    g_assert_true(pu_write_raw("data/root.ext4", g_file_get_path(fixture->file),
                   &device, 2, 0, 0, &fixture->error));
     g_assert_no_error(fixture->error);
 }
@@ -190,11 +190,11 @@ main(int argc,
     g_test_add_func("/utils/file_copy", test_file_copy);
     g_test_add_func("/utils/file_copy_fail", test_file_copy_fail);
     g_test_add_func("/utils/archive_extract", test_archive_extract);
-    g_test_add("/utils/make_filesystem", EmptyFileFixture, NULL, empty_file_set_up,
+    g_test_add("/utils/make_filesystem", EmptyFileFixture, "file", empty_file_set_up,
                test_make_filesystem, empty_file_tear_down);
-    g_test_add("/utils/write_raw", EmptyFileFixture, NULL, empty_file_set_up,
+    g_test_add("/utils/write_raw", EmptyFileFixture, "file", empty_file_set_up,
                test_write_raw, empty_file_tear_down);
-    g_test_add("/utils/write_raw_input_offset", EmptyFileFixture, NULL, empty_file_set_up,
+    g_test_add("/utils/write_raw_input_offset", EmptyFileFixture, "file", empty_file_set_up,
                test_write_raw_input_offset, empty_file_tear_down);
     g_test_add_func("/utils/path_from_uri", test_path_from_uri);
     g_test_add_func("/utils/path_from_uri_empty", test_path_from_uri_empty);
