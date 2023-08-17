57a672c5f1f5e5b768fe5056ac4a902b8f9c745e
Martin Schwan <m.schwan@phytec.de>
2023-08-17 13:32:51 +0200

    src: command: Allow passing command usage
    Allow passing a command usage overview by using the special option entry
    G_OPTION_REMAINING and its argument description field.
    
    The parsing of arguments can now only be executed once. Parsing
    arguments multiple times was not properly supported before anyway, so
    prevent using it now.
    
    The internal argument parsing now does not change argv anymore and only
    uses the command context's internal args variable. This is also to
    reduce confusion in using these functions to outside variables.
    
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

--- src/pu-command.c
+++ src/pu-command.c
@@ -11,6 +11,7 @@ struct _PuCommandContext {
     PuCommandEntry *command;
     /* Additional arguments (null-terminated), not being commands or options */
     gchar **args;
+    gsize args_len;
 
     /* The option context to append options to */
     GOptionContext *option_context;
@@ -21,6 +22,9 @@ struct _PuCommandContext {
     /* All valid command entries */
     PuCommandEntry *entries;
     gsize entries_len;
+
+    /* If some input args were already parsed */
+    gboolean parsed;
 };
 
 G_DEFINE_QUARK(pu-command-context-error-quark, pu_command_error)
@@ -31,7 +35,8 @@ pu_command_context_new(void)
     PuCommandContext *context;
 
     context = g_new0(PuCommandContext, 1);
-    context->option_context = g_option_context_new("COMMAND");
+    context->option_context = g_option_context_new(NULL);
+    context->parsed = FALSE;
 
     return context;
 }
@@ -229,10 +234,13 @@ gboolean
 pu_command_context_parse(PuCommandContext *context,
                          gint *argc,
                          gchar ***argv,
+                         gchar ***arg_remaining,
                          GError **error)
 {
     g_return_val_if_fail(context != NULL, FALSE);
+    g_return_val_if_fail(arg_remaining != NULL && *arg_remaining == NULL, FALSE);
     g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+    g_return_val_if_fail(!context->parsed, FALSE);
 
     if (!argc || !argv) {
         g_set_error(error, PU_COMMAND_ERROR, PU_COMMAND_ERROR_BAD_VALUE,
@@ -270,6 +278,17 @@ pu_command_context_parse(PuCommandContext *context,
     if (context->command && context->command->option_entries != NULL) {
         g_option_context_add_main_entries(context->option_context,
                                           context->command->option_entries, NULL);
+    } else {
+        /* Main option entries should not contain the G_OPTION_REMAINING, as it
+         * would override the one provided with the command specific one. For
+         * that reason, add the main usage here. */
+        GOptionEntry extra_entries[] = {
+            { G_OPTION_REMAINING, 0, G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING_ARRAY,
+                arg_remaining, "", "COMMAND" },
+            { NULL }
+        };
+        g_option_context_add_main_entries(context->option_context,
+                                          extra_entries, NULL);
     }
 
     /* Parse main options and command-specific ones */
@@ -278,30 +297,29 @@ pu_command_context_parse(PuCommandContext *context,
         return FALSE;
     }
 
-    /* Consume first argument (program name) and second one (command name) and
-     * update argv accordingly */
-    for (gint i = 0; i < *argc - 2; i++) {
-        g_free((*argv)[i]);
-        (*argv)[i] = g_strdup((*argv)[i + 2]);
-    }
-    for (gint i = 0; i < 2; i++) {
-        g_free((*argv)[*argc - 1]);
-        (*argv)[*argc - 1] = NULL;
-        (*argc)--;
+    /* Retrieve remaining arguments */
+    context->args_len = g_strv_length(*arg_remaining) - 1;
+    context->args = g_malloc0((context->args_len + 1) * sizeof(gchar *));
+    if (context->args_len > 0) {
+        /* First remaining argument is the command, skip that one */
+        for (gsize i = 0; i < context->args_len; i++)
+            context->args[i] = g_strdup((*arg_remaining)[i + 1]);
     }
 
     /* Check for correct number of arguments depending on command type */
-    if ((*argc != 0 && context->command->arg == PU_COMMAND_ARG_NONE) ||
-        (*argc != 1 && context->command->arg == PU_COMMAND_ARG_FILENAME) ||
-        (*argc < 2 && context->command->arg == PU_COMMAND_ARG_FILENAME_ARRAY)) {
-        g_autofree gchar *excess_args = g_strjoinv(" ", *argv + 1);
+    if ((context->args_len != 0 && context->command->arg == PU_COMMAND_ARG_NONE) ||
+        (context->args_len != 1 && context->command->arg == PU_COMMAND_ARG_FILENAME) ||
+        (context->args_len < 2 && context->command->arg == PU_COMMAND_ARG_FILENAME_ARRAY)) {
+        g_autofree gchar *excess_args = NULL;
+        if (context->args)
+            excess_args = g_strjoinv(" ", context->args);
         g_set_error(error, PU_COMMAND_ERROR, PU_COMMAND_ERROR_BAD_VALUE,
-                    "Invalid number of arguments for command '%s'",
-                    context->command->name);
+                    "Invalid number of arguments (%ld) for command '%s': '%s'",
+                    context->args_len, context->command->name, excess_args);
         return FALSE;
     }
 
-    context->args = g_strdupv(*argv);
+    context->parsed = TRUE;
 
     return TRUE;
 }
@@ -309,16 +327,18 @@ pu_command_context_parse(PuCommandContext *context,
 gboolean
 pu_command_context_parse_strv(PuCommandContext *context,
                               gchar ***args,
+                              gchar ***arg_remaining,
                               GError **error)
 {
     gboolean res;
     gint argc;
 
     g_return_val_if_fail(context != NULL, FALSE);
+    g_return_val_if_fail(arg_remaining != NULL && *arg_remaining == NULL, FALSE);
     g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
 
     argc = args && *args ? g_strv_length(*args) : 0;
-    res = pu_command_context_parse(context, &argc, args, error);
+    res = pu_command_context_parse(context, &argc, args, arg_remaining, error);
 
     return res;
 }
