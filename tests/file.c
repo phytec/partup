ea16ac4c5dd0c22261a9e771f4b295cd51af0f88
Martin Schwan <m.schwan@phytec.de>
2024-10-09 14:19:35 +0200

    tests: Create new module "file" for file-related unit tests
    Create a new module "file" for all file-related unit tests and use the
    new file module from the sources for it.
    
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

---
+++ tests/file.c
@@ -0,0 +1,76 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (c) 2024 PHYTEC Messtechnik GmbH
+ */
+
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <gio/gio.h>
+#include "pu-file.h"
+#include "pu-error.h"
+
+#define ROOT_EXT4_SIZE 262144
+
+static void
+test_file_copy(void)
+{
+    g_autoptr(GError) error = NULL;
+    g_autofree gchar *dest = NULL;
+    g_autofree gchar *out_file = NULL;
+    const gchar *source = "data/lorem.txt";
+
+    dest = g_dir_make_tmp("partup-XXXXXX", &error);
+    g_assert_no_error(error);
+
+    out_file = g_build_filename(dest, "lorem.txt", NULL);
+
+    g_assert_true(pu_file_copy(source, dest, &error));
+    g_assert_no_error(error);
+    g_assert_true(g_file_test(out_file, G_FILE_TEST_IS_REGULAR));
+
+    g_assert_cmpint(g_remove(out_file), ==, 0);
+    g_assert_cmpint(g_rmdir(dest), ==, 0);
+}
+
+static void
+test_file_copy_fail(void)
+{
+    g_autoptr(GError) error = NULL;
+    g_autofree gchar *dest = NULL;
+    g_autofree gchar *out_file = NULL;
+    const gchar *source = "data/thisfilereallydoesnotexist";
+
+    dest = g_dir_make_tmp("partup-XXXXXX", &error);
+    g_assert_no_error(error);
+
+    g_assert_false(pu_file_copy(source, dest, &error));
+    g_assert_error(error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND);
+
+    g_assert_cmpint(g_rmdir(dest), ==, 0);
+}
+
+static void
+test_file_get_size(void)
+{
+    g_autoptr(GError) error = NULL;
+    goffset size = pu_file_get_size("data/root.ext4", &error);
+    g_assert_no_error(error);
+    g_assert_cmpuint(size, ==, ROOT_EXT4_SIZE);
+}
+
+int
+main(int argc,
+     char *argv[])
+{
+    g_test_init(&argc, &argv, NULL);
+
+#ifdef PARTUP_TEST_SRCDIR
+    g_chdir(PARTUP_TEST_SRCDIR);
+#endif
+
+    g_test_add_func("/file/file_copy", test_file_copy);
+    g_test_add_func("/file/file_copy_fail", test_file_copy_fail);
+    g_test_add_func("/file/file_get_size", test_file_get_size);
+
+    return g_test_run();
+}
--- tests/utils.c
+++ tests/utils.c
@@ -11,46 +11,6 @@
 #include "pu-utils.h"
 #include "pu-error.h"
 
-#define ROOT_EXT4_SIZE 262144
-
-static void
-test_file_copy(void)
-{
-    g_autoptr(GError) error = NULL;
-    g_autofree gchar *dest = NULL;
-    g_autofree gchar *out_file = NULL;
-    const gchar *source = "data/lorem.txt";
-
-    dest = g_dir_make_tmp("partup-XXXXXX", &error);
-    g_assert_no_error(error);
-
-    out_file = g_build_filename(dest, "lorem.txt", NULL);
-
-    g_assert_true(pu_file_copy(source, dest, &error));
-    g_assert_no_error(error);
-    g_assert_true(g_file_test(out_file, G_FILE_TEST_IS_REGULAR));
-
-    g_assert_cmpint(g_remove(out_file), ==, 0);
-    g_assert_cmpint(g_rmdir(dest), ==, 0);
-}
-
-static void
-test_file_copy_fail(void)
-{
-    g_autoptr(GError) error = NULL;
-    g_autofree gchar *dest = NULL;
-    g_autofree gchar *out_file = NULL;
-    const gchar *source = "data/thisfilereallydoesnotexist";
-
-    dest = g_dir_make_tmp("partup-XXXXXX", &error);
-    g_assert_no_error(error);
-
-    g_assert_false(pu_file_copy(source, dest, &error));
-    g_assert_error(error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND);
-
-    g_assert_cmpint(g_rmdir(dest), ==, 0);
-}
-
 static void
 test_archive_extract(void)
 {
@@ -202,15 +162,6 @@ test_device_get_partition_path_fail(void)
 }
 
 static void
-test_get_file_size(void)
-{
-    g_autoptr(GError) error = NULL;
-    goffset size = pu_get_file_size("data/root.ext4", &error);
-    g_assert_no_error(error);
-    g_assert_cmpuint(size, ==, ROOT_EXT4_SIZE);
-}
-
-static void
 test_str_pre_remove(void)
 {
     g_autofree gchar *in = g_strdup("partup");
@@ -253,8 +204,6 @@ main(int argc,
     g_chdir(PARTUP_TEST_SRCDIR);
 #endif
 
-    g_test_add_func("/utils/file_copy", test_file_copy);
-    g_test_add_func("/utils/file_copy_fail", test_file_copy_fail);
     g_test_add_func("/utils/archive_extract", test_archive_extract);
     g_test_add("/utils/make_filesystem", EmptyFileFixture, "file", empty_file_set_up,
                test_make_filesystem, empty_file_tear_down);
@@ -272,8 +221,6 @@ main(int argc,
                     test_device_get_partition_path_sd);
     g_test_add_func("/utils/device_get_partition_path_fail",
                     test_device_get_partition_path_fail);
-    g_test_add_func("/utils/get_file_size",
-                    test_get_file_size);
     g_test_add_func("/utils/str_pre_remove", test_str_pre_remove);
     g_test_add_func("/utils/device_get_partition_pattern", test_device_get_partition_pattern);
 
